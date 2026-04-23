6d53b7e120947e01f502d800618231ed97125b8d
eslams3dawi <eslamseadawy717@gmail.com>
2026-04-23 15:17:23 +0200

    Add Square Shape

---
+++ Drawing Square/Square practice/main.cpp
@@ -0,0 +1,20 @@
+#include <GL/glut.h>
+#include <windows.h>
+
+
+void myInit()//
+{
+    glClearColor(1,0,0,0);
+    glPointSize(5);
+    gluOrtho2D();
+}
+
+void displayFunction()
+{
+
+}
+
+int main(int argc, char** argv)
+{
+
+}
